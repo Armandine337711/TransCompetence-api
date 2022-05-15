9512cf739882916a37358a98d426b7fd43588150
Armandine BARGE <armandine.barge@ntymail.com>
2022-05-15 09:26:31 +0200

    Setup generic CRUD

---
+++ app/controllers/genericController.js
@@ -0,0 +1,93 @@
+const genericModel = require("../Model/genericModel");
+const bcrypt = require('bcrypt')
+
+const genericController = {
+    async getAll(request, response, next) {
+        try {
+            const { entity } = request.params;
+            const data = await genericModel.getAll(entity);
+
+            if (data) {
+                response.json({ data });
+            } else {
+                next();
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    async getOne(request, response, next) {
+        try {
+            const { entity, id } = request.params;
+            const data = await genericModel.getOne(entity, id);
+            if (data) {
+                response.json({ data });
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    async createOne(request, response, next) {
+        try {
+            const { entity } = request.params;
+            const newDatas = request.body;
+
+            if (entity === 'member') {
+                const hashedPassword = bcrypt.hashSync(newDatas.password, 10);
+                newDatas.password = hashedPassword;
+            }
+
+            const data = await genericModel.createOne(entity, newDatas);
+            if (data) {
+                response.json({ data });
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    async updateOne(request, response, next) {
+
+        try {
+            const { entity, id } = request.params;
+            const updatedDatas = request.body;
+            if (parseInt(id) == parseInt(updatedDatas.id)) {
+                const data = await genericModel.updateOne(entity, updatedDatas);
+                if (data) {
+                    response.json({ data })
+                }
+            } else {
+                next()
+            }
+
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async deleteAll(request, response, next) {
+        try {
+            const { entity } = request.params;
+            await genericModel.deleteAll(entity);
+            return 'Suppression effectuée'
+
+        } catch (error) {
+            next(error)
+
+        }
+    },
+
+    async deleteOne(request, response, next) {
+        try {
+            const { entity, id } = request.params;
+            await genericModel.deleteOne(entity, id);
+            return 'suppression effectuée'
+        } catch (error) {
+            next(error)
+        }
+    }
+};
+
+module.exports = genericController;
