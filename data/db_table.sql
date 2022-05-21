9ba9c2b429f39270a509aaa70f52b6a7a148198c
Armandine BARGE <armandine.barge@ntymail.com>
2022-05-21 18:02:45 +0200

    db functions in course

---
+++ data/db_table.sql
@@ -0,0 +1,160 @@
+BEGIN;
+-------------------------------
+-- EXTENTION & DOMAIN
+-------------------------------
+
+DROP EXTENSION IF EXISTS unaccent;
+
+CREATE EXTENSION unaccent;
+
+DROP DOMAIN IF EXISTS TEXT_ONLY, ALPHANUM, TEXT_MAIL;
+
+CREATE DOMAIN TEXT_ONLY AS TEXT CHECK(unaccent(VALUE) ~ '^[A-Za-z \-]+$');
+CREATE DOMAIN ALPHANUM AS TEXT CHECK(unaccent(VALUE) ~ '^[A-Za-z\ \-\#\d]+$');
+CREATE DOMAIN TEXT_MAIL AS TEXT CHECK(VALUE ~ '(^[a-z\d\.\-\_]+)@{1}([a-z\d\.\-]{2,})[.]([a-z]{2,5})$');
+
+-------------------------------
+-- TABLES
+-------------------------------
+DROP TABLE IF EXISTS "position", "member", "client";
+
+CREATE TABLE IF NOT EXISTS "position"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"entitled" TEXT_ONLY NOT NULL UNIQUE
+);
+
+INSERT INTO "position"("entitled") VALUES (
+('Contrôleur de Gestion'),
+('Administrateur'),
+('Commercial'),
+('Db_Concept')
+);
+
+CREATE TABLE IF NOT EXISTS "member"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"firstname" TEXT_ONLY NOT NULL,
+"lastname" TEXT_ONLY NOT NULL,
+"email" TEXT_MAIL NOT NULL UNIQUE,
+"pwd" TEXT NOT NULL,
+"position_id" INT REFERENCES "position"("id") DEFAULT 3,
+"createdAt" TIMESTAMPSTZ DEFAULT NOW(),
+"updatedAt" TIMESTAMPSTZ DEFAULT NOW(),
+"last_connection" TIMESTAMPSTZ
+);
+
+CREATE TABLE IF NOT EXISTS "client"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"business_name" ALPHANUM NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "loading_unit"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"unit" TEXT_ONLY NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "financing_method"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"method" TEXT_ONLY NOT NULL UNIQUE
+);
+
+INSERT INTO "financing_method"("method") VALUES (
+    ('Emprunt'),
+    ('Crédit Bail'),
+    ('Location financière');
+)
+
+CREATE TABLE IF NOT EXISTS "financial_datas"(
+    "id" INT GENERATED ALWAYS AS IDENTITY KEY,
+    "yearly_mileage" INT NOT NULL,
+    "in_charge_mileage" INT NOT NULL -- <= yearly
+    "nb_towed_vehicles" INT -- 0 ou 1
+    "nb_days_in_operation" INT -- <365
+    "loading_unit_id" INT NOT NULL REFERENCES "loading_unit"("id"),
+    "loading_capacity" INT,
+    "capacity_utilisation_factor" FLOAT, -- >100
+    "gazoline_consumption" FLOAT,
+    "average_price_liter" FLOAT,
+    "proportion_tanker_supply" FLOAT, -- <100
+    "average_price_liter_tanker" FLOAT,
+    "km_cost_tyres" FLOAT,
+    "nb_tyres_motor_vehicle" INT,
+    "nb_tyres_towed_vehicle" INT,
+    "price_tyre_motor_vehicle" FLOAT,
+    "price_tyre_towed_vehicle" FLOAT,
+    "lifetime_motor_vehicle" INT, -- ou float ?
+    "lifetime_towed_vehicle" INT,
+    "yearly_maintenance_cost" FLOAT,
+    "yearly_toll_cost", FLOAT -- trad toll = péage
+    "duration_motor_vehicle_use" INT -- ou float ?
+    "motor_vehicle_loading_unit_id" INT REFERENCES "loading_unit"("id"), -- 1 2 ou 3  default valeur ?
+    "value_motor_vehicle" FLOAT, --ou INT
+    "motor_vehicle_loan_amount" FLOAT, -- si emprunt
+    "motor_vehicle_borrowing_rate" FLOAT, -- <100 si emprunt
+    "motor_vehicle_loan_duration" INT, -- si emprunt
+    "mv_resale_value" FLOAT, -- si emprunt
+    "mv_contract_length" INT -- si LLD ou crédit bail
+    "mv_monthly_rental_amount" FLOAT,
+    "optional purchase value" FLOAT,
+    "duration_towed_vehicle" INT,
+    "towed_vehicle_loading_unit_id" INT REFERENCES "loading_unit"("id"),
+    "value_towed_vehicle" FLOAT,
+    "towed_vehicle_loan_amount" FLOAT,
+    "towed_vehicle_borrowing_rate" FLOAT,
+    "towed_vehicle_loan_duration" INT,
+    "tv_resale_value" FLOAT,
+    "tv_contract_length" INT,
+    "mv_monthly_rental_amount" FLOAT,
+    "yearly_insurance_amount" FLOAT,
+    "yearly_goods_carried_insurance_amount" FLOAT,
+    "yearly_axle_tax" FLOAT,
+    "yearly structural_cost" FLOAT,
+    "nb_driver_per_vehicle" INT,-- >=1
+    "yearly_activity_driver" INT,
+    "monthly_working_time" INT,
+    "monthly_driving_time" INT,
+    "monthly_driver_salary" FLOAT,
+    "yearly_bonuses" FLOAT,
+    "employment_contribution_rate" FLOAT,
+    "yearly_travel_allowance" FLOAT,
+    "nb_paid_month" INT,
+    "cost_component_label" TEXT,
+    "cost_component_unit" TEXT,
+    "nb_trying_unit" INT,
+    "yearly_amount_cist_component" FLOAT,
+    "validated" BOOLEAN DEFAULT false,
+    "in_course" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS "cnr_rating"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"rating_date" TIMESTAMPSTZ UNIQUE NOT NULL,
+"frozen_fridge_ld_ea" FLOAT,
+"professionnal_fuel" FLOAT,
+"service" FLOAT,
+"insfrastructure" FLOAT,
+"structural_costs" FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS "costing"(
+"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+"quotation_date" TIMESTAMPSTZ NOT NULL,
+"member_id" INT NOT NULL REFERENCES "member"("id"),
+"client_id" INT NOT NULL REFERENCES "client"("id"),
+"AB_distance" FLOAT, -- ou INT
+"AB_toll" FLOAT,
+"AB_duration" FLOAT,
+"B_loading_time" FLOAT,
+"BC_distance" FLOAT,
+"BC_toll" FLOAT,
+"BC_duration" FLOAT,
+"C_unloading_time" FLOAT,
+"CA_distance" FLOAT,
+"CA_toll" FLOAT,
+"CA_duration" FLOAT,
+"loading_unit_id" INT REFERENCES "loading_unit"("id"),
+"quantity_loading_unit" FLOAT,
+"dayly_working_time" FLOAT,
+
+);
+
+COMMIT;
---
+++ data/db_update_functions.sql
@@ -0,0 +1,136 @@
+BEGIN;
+
+-------------------------------
+-- EXAMPLE CREATE FUNCTION UPDATE
+-------------------------------
+DROP FUNCTION IF EXISTS "member_update", connection_update, client_update, loading_unit_update, financial_datas_update, validate_financial_datas_update, in_course_financial_datas_update ;
+
+CREATE FUNCTION "member_update"(updatedDatas json) RETURNS SETOF "member" AS
+    $$
+    UPDATE "member" SET
+    "firstname" = updatedDatas ->> 'firstname',
+    "lastname" = updatedDatas ->> 'lastname',
+    "email" = updatedDatas ->> 'email',
+    "pwd" = updatedDatas ->> 'pwd',
+    "position_id" = (updatedDatas ->> 'position_id')::int
+    "updatedAt" = NOW()
+    WHERE "id" = (updatedDatas ->> 'id')::int
+    RETURNING *;
+    $$
+LANGUAGE sql VOLATILE STRICT;
+
+-- update the last connection of the member
+CREATE FUNCTION "connection_update"() RETURNS VOID AS
+    $$
+    UPDATE "member" SET
+    "last_connection" = NOW()
+    WHERE "id" = (updatedDatas ->> 'id')::int;
+    $$
+LANGUAGE sql VOLATILE STRICT;
+
+CREATE FUNCTION "client_update"(updatedDatas json) RETURNS SETOF "client" AS
+    $$
+    UPDATE "client" SET
+    "business_name" = updatedDatas ->> 'business_name'
+    WHERE "id" = (updatedDatas ->> 'id')::int
+    RETURNING *;
+    $$
+LANGUAGE sql VOLATILE STRICT;
+
+CREATE FUNCTION "loading_unit_update"(updatedDatas json) RETURNS SETOF "loading_unit" AS
+    $$
+    UPDATE "loading_unit" SET
+    "unit" = updataedDatas ->> 'unit'
+    WHERE "id" = (updatedDatas ->> 'id')::int
+    RETURNING *;
+    $$
+LANGUAGE sql VOLATILE STRICT;
+
+CREATE FUNCTION "financial_datas_update"(updatedDatas json) RETURNS SETOF "financial_datas" AS
+$$
+UPDATE "financial_datas" SET
+    "yearly_mileage" = (updatedDatas ->>'yearly_mileage')::int,
+    "in_charge_mileage" = (updatedDatas ->>'in_charge_mileage')::int,
+    "nb_towed_vehicles" = (updatedDatas ->>'nb_towed_vehicles')::int,
+    "nb_days_in_operation" = (updatedDatas ->>'nb_days_in_operation')::int,
+    "loading_unit_id" = (updatedDatas ->>'loading_unit_id')::int,
+    "loading_capacity" = (updatedDatas ->>'loading_capacity')::int,
+    "capacity_utilisation_factor" = (updatedDatas ->>'capacity_utilisation_factor')::float,
+    "gazoline_consumption" = (updatedDatas ->>'gazoline_consumption')::float,
+    "average_price_liter" = (updatedDatas ->>'average_price_liter')::float,
+    "proportion_tanker_supply" = (updatedDatas ->>'proportion_tanker_supply')::float,
+    "average_price_liter_tanker" = (updatedDatas ->>'average_price_liter_tanker')::float,
+    "km_cost_tyres" = (updatedDatas ->>'km_cost_tyres')::float,
+    "'nb_tyres_motor_vehicle" = (updatedDatas ->>'nb_tyres_motor_vehicle')::int,
+    "nb_tyres_towed_vehicle" = (updatedDatas ->>'nb_tyres_towed_vehicle')::int,
+    "price_tyre_motor_vehicle" = (updatedDatas ->>'price_tyre_motor_vehicle')::float,
+    "price_tyre_towed_vehicle" = (updatedDatas ->>'price_tyre_towed_vehicle')::float,
+    "lifetime_motor_vehicle" = (updatedDatas ->>'lifetime_motor_vehicle')::int,
+    "lifetime_towed_vehicle" = (updatedDatas ->>'lifetime_towed_vehicle')::int,
+    "yearly_maintenance_cost" = (updatedDatas ->>'yearly_maintenance_cost')::int,
+    "yearly_toll_cost" = (updatedDatas ->>'yearly_toll_cost')::float,
+    "duration_motor_vehicle_use" = (updatedDatas ->>'duration_motor_vehicle_use')::int,
+    "motor_vehicle_loading_unit_id" = (updatedDatas ->>'motor_vehicle_loading_unit_id')::int,
+    "alue_motor_vehicle" = (updatedDatas ->>'value_motor_vehicle')::int,
+    "motor_vehicle_loan_amount" = (updatedDatas ->>'motor_vehicle_loan_amount')::float,
+    "motor_vehicle_borrowing_rate" = (updatedDatas ->>'motor_vehicle_borrowing_rate')::float,
+    "motor_vehicle_loan_duration" = (updatedDatas ->>'motor_vehicle_loan_duration')::int,
+    "mv_resale_value" = (updatedDatas ->>'mv_resale_value')::float,
+    "mv_contract_length" = (updatedDatas ->>'mv_contract_length')::int,
+    "mv_monthly_rental_amount" = (updatedDatas ->>'mv_monthly_rental_amount')::float,
+    "optional purchase value" = (updatedDatas ->>'optional purchase value')::float,
+    "duration_towed_vehicle" = (updatedDatas ->>'duration_towed_vehicle')::int,
+    "towed_vehicle_loading_unit_id" = (updatedDatas ->>'towed_vehicle_loading_unit_id')::int,
+    "value_towed_vehicle" = (updatedDatas ->>'value_towed_vehicle')::float,
+    "towed_vehicle_loan_amount" = (updatedDatas ->>'towed_vehicle_loan_amount')::float,
+    "towed_vehicle_borrowing_rate" = (updatedDatas ->>'towed_vehicle_borrowing_rate')::float,
+    "towed_vehicle_loan_duration" = (updatedDatas ->>'towed_vehicle_loan_duration')::float,
+    "tv_resale_value" = (updatedDatas ->>'tv_resale_value')::float,
+    "tv_contract_length" = (updatedDatas ->>'tv_contract_length'):int,
+    "mv_monthly_rental_amount" = (updatedDatas ->>'mv_monthly_rental_amount'):float,
+    "yearly_insurance_amount" = (updatedDatas ->>'yearly_insurance_amount'):float,
+    "yearly_goods_carried_insurance_amount" = (updatedDatas ->>'yearly_goods_carried_insurance_amount'):float,
+    "yearly_axle_tax" = (updatedDatas ->>'yearly_axle_tax')::float,
+    "yearly structural_cost" = (updatedDatas ->>'yearly structural_cost')::float,
+    "nb_driver_per_vehicle" = (updatedDatas ->>'nb_driver_per_vehicle')::float,
+    "yearly_activity_driver" = (updatedDatas ->>'yearly_activity_driver')::int,
+    "monthly_working_time" = (updatedDatas ->>'monthly_working_time')::int,
+    "monthly_driving_time" = (updatedDatas ->>'monthly_driving_time')::int,
+    "monthly_driver_salary" = (updatedDatas ->>'monthly_driver_salary')::float,
+    "yearly_bonuses" = (updatedDatas ->>'yearly_bonuses')::float,
+    "employment_contribution_rate" = (updatedDatas ->>'employment_contribution_rate')::float,
+    "yearly_travel_allowance" = (updatedDatas ->>'yearly_travel_allowance')::float,
+    "nb_paid_month" = (updatedDatas ->>'nb_paid_month')::int,
+    "cost_component_label" = updatedDatas ->>'cost_component_label',
+    "cost_component_unit" = updatedDatas ->>'cost_component_unit',
+    "nb_trying_unit" = (updatedDatas ->>'nb_trying_unit')::int,
+    "yearly_amount_cist_component" = (updatedDatas ->>'yearly_amount_cist_component')::float
+    WHERE "id" = (updatedDatas ->> 'id')::int
+ RETURNING *;
+$$
+LANGUAGE sql VOLATILE STRICT;
+
+CREATE FUNCTION "validate_financial_datas_update"("id") RETURNS VOID AS
+    $$
+    UPDATE "financial_datas" SET
+    "validated" = true
+    WHERE "id" = 'id'
+    RETURNING *;
+    $$
+LANGUAGE sql VOLATILE STRICT;
+
+CREATE FUNCTION "in_course_financial_datas_update"(id) RETURNS SETOF "in_course_financial_datas" AS
+$$
+UPDATE "financial_datas" SET
+"in_course" = true
+WHERE "id" = 'id'
+$$
+LANGUAGE VOLATILE STRICT;
+
+UPDATE "financial_datas" SET
+"in_course" = false
+WHERE "id" <> 'id'
+$$
+LANGUAGE sql VOLATILE STRICT;
+
+COMMIT;
