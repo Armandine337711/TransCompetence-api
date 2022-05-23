b70ab28b40052f504e4056b5c8b2b7625785d165
Armandine BARGE <armandine.barge@ntymail.com>
2022-05-23 22:43:27 +0200

    fix mistake cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const router = require('./app/router');
 const app = express();
 
 app.use(express.json());
-app.use(cors('*'));
+app.use(cors());
 app.use(router);
 
 app.listen(process.env.PORT || 3000, () => {
