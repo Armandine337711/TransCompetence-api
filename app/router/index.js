e0126f3c70bb1da75f6e72213955afa24c204209
Armandine BARGE <armandine.barge@ntymail.com>
2022-05-24 21:09:45 +0200

    update routes

--- app/router/index.js
+++ app/router/index.js
@@ -9,14 +9,14 @@ const router = express.Router();
 /* generic CRUD */
 
 
-router.get('/position', positionController.getAll)
+// router.get('/position', positionController.getAll)
 
-// router.get('/:entity', genericController.getAll);
-// router.get('/:entity/:id', genericController.getOne);
-// router.post('/:entity', genericController.createOne);
-// router.patch('/:entity/:id', genericController.updateOne);
-// router.delete('/:entity', genericController.deleteAll);
-// router.delete('/:entity/:id', genericController.deleteOne);
+router.get('/:entity', genericController.getAll);
+router.get('/:entity/:id', genericController.getOne);
+router.post('/:entity', genericController.createOne);
+router.patch('/:entity/:id', genericController.updateOne);
+router.delete('/:entity', genericController.deleteAll);
+router.delete('/:entity/:id', genericController.deleteOne);
 router.get('/', genericController.home);
 // router.use(errorController.error404);
 // router.use(errorController.error500);
