685279028523c0f8aca5e10b5e83387f96a78d66
Armandine BARGE <armandine.barge@ntymail.com>
2022-06-08 21:27:00 +0200

    fix cors 1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }))
-app.use(cors('*'));
+app.use(cors());
 app.use(router);
 
 app.listen(process.env.PORT || 3000, () => {
